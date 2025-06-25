516abbd9ad8ed98cfecfb4565047971b2af7be18
Salman790753 <sfarshi222@gmail.com>
2025-06-25 13:21:15 +0600

    solved

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolce, resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                news:resolve(__dirname, "news.html");
+            }
+        }
+    }
+})
